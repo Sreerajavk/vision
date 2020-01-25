f22b029c864effa1aeb224f08a2f55328f278456
sreeraj <sreerajavk123@gmail.com>
2020-01-25 12:17:19 +0530

    dashboard done

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+
+#for redirect to dashboard
+def index(request):
+    return redirect('/dashboard')
+
+#the dashboard page
+def dashboard(request):
+    return render(request , 'dashboard.html' , {})
