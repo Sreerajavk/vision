3221972a830babab26c9dfc2d5b55168739bda3f
sreeraj <sreerajavk123@gmail.com>
2020-01-29 20:33:06 +0530

    db changed in production mode

--- vision/settings.py
+++ vision/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(pdsn6#gk_a6wh!+key9nyn_h+9p!h@(8x3f8mi_&@4f!ri#@t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -89,6 +89,14 @@ DATABASES = {
     }
 }
 
+if not DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -128,6 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR , 'static'),)
+STATIC_ROOT = (os.path.join(BASE_DIR , 'static'))
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
