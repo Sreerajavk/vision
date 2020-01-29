204a099056ba13d0c1249ba61c7f8c895e0e2eb1
sreeraj <sreerajavk123@gmail.com>
2020-01-29 22:00:57 +0530

    home page modified

--- vision/settings.py
+++ vision/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(pdsn6#gk_a6wh!+key9nyn_h+9p!h@(8x3f8mi_&@4f!ri#@t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']
 
@@ -96,7 +97,7 @@ if not DEBUG:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
+    STATIC_ROOT = (os.path.join(BASE_DIR, 'static'))
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -136,7 +137,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR , 'static'),)
-STATIC_ROOT = (os.path.join(BASE_DIR , 'static'))
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
